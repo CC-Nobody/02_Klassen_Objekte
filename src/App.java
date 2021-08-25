5709ae209410d64ea2a1bf9e77dc45644d2f7d30
CC-Nobody <cc.adiermann@gmail.com>
2021-08-25 11:25:18 +0200

    Changes 11am 25.08.2021

--- src/App.java
+++ src/App.java
@@ -2,13 +2,50 @@ public class App {
     
     public static void main(String[] args) {
         
-    Cat cat  = new Cat(); 
+    Cat cat1  = new Cat("Grizabella","White",83);
+    // cat1.changeYourName("Grizabella");
+    // cat1.dyeYourFur("White");
+    // cat1.setAge(83);
+
+    ausgabe("RAM-Adresse: " + cat1.tellYourAddress());
+    ausgabe("Name: " + cat1.tellYourName());
+    ausgabe("Farbe: " + cat1.tellYourColor());
+    ausgabe("Alter: " + cat1.tellYourAge());
+    ausgabe("Alter: " + cat1.tellYourAlter());
+    // ausgabe(cat1.name);
     
-    ausgabe("Blick von aussen: " + cat);
-    ausgabe("Blick von innen: " + cat.tellYourAddress());
-    // cat.tellYourAddress();
-   
+    // ausgabe("Blick von aussen: " + cat1);
+    // ausgabe("Blick von innen: " + cat1.tellYourAddress());
+
+    ausgabe("");
+    ausgabe("--------------------");
+    ausgabe("");
+
+    Cat cat2  = new Cat("Alonzo","Brown",42); 
+    // cat2.changeYourName("Alonzo");
+    // cat2.dyeYourFur("Brown");
+    // cat2.setAge(42);
+
+    ausgabe("RAM-Adresse: " + cat2.tellYourAddress());
+    ausgabe("Name: " + cat2.tellYourName());
+    ausgabe("Farbe: " + cat2.tellYourColor());
+    ausgabe("Alter: " + cat2.tellYourAge());
+    // ausgabe(cat2.name);
+
+    ausgabe("");
+    ausgabe("--------------------");
+    ausgabe("");
+  
+    Cat cat3 = new Cat("Blitzo","Red",32);
+    ausgabe("RAM-Adresse: " + cat3.tellYourAddress());
+    ausgabe("Name: " + cat3.tellYourName());
+    ausgabe("Farbe: " + cat3.tellYourColor());
+    ausgabe("Alter: " + cat3.tellYourAge());
+
+    // ausgabe("Blick von aussen: " + cat2);
+    // ausgabe("Blick von innen: " + cat2.tellYourAddress());
     }
+
 
     public static void ausgabe(String outputStr) {
       System.out.println(outputStr);  
--- src/Cat.java
+++ src/Cat.java
@@ -1,10 +1,52 @@
 public class Cat {
     
-
-    Cat tellYourAddress(){
-        return this;
+    // Attribut | Field | Property
+    private String name = "nobody";
+    private String furColor;
+    private int age;
+    
+    // Constructor
+    Cat(String name, String furColor, int age){
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
     }
 
 
+
+    public Cat tellYourAddress(){
+        return this;
+    }
+
+    String tellYourName(){
+        return this.name;
+    }
+
+    public void changeYourName(String name){
+        this.name = name;
+    }
+
+    String tellYourColor(){
+        return this.furColor;
+    }
     
+    public void dyeYourFur(String furColor){
+        this.furColor = furColor;
+    }
+
+    Integer tellYourAge(){
+        return this.age;
+    }
+
+    public void setAge(Integer age){
+        this.age = age;
+    }
+
+    // Age String
+    String alter = Integer.toString(age);
+    String alter2 = "" + age;
+
+    String tellYourAlter(){
+        return String.valueOf(age);
+    }
 }
